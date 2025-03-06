491ca22ba3efe1b4df503740ca87a8e6aedd276e
Cam Eadie <cam.eadie@gmail.com>
2025-03-06 07:41:03 +0000

    Final main improvement

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #from kf_simulation import simulate_kalman_filter_1d, simulate_kalman_filter_2d
 #simulate_kalman_filter_1d()
 
-from scapy.all import sniff, Dot11Beacon, Dot11ProbeReq
+from scapy.all import sniff, Dot11Beacon, Dot11ProbeReq, Dot11ProbeResp
 
 def packet_callback(packet):
-    print("sniff")
-    if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeReq):
-        rssi = getattr(packet, "dBm_AntSignal", "N/A")  # Handle missing RSSI
-        ssid = getattr(packet, "info", b"Hidden SSID").decode(errors="ignore")  # Handle missing SSID
-        mac = packet.addr2
+    rssi = getattr(packet, "dBm_AntSignal", "N/A")  # Handle missing RSSI
+    ssid = getattr(packet, "info", b"Hidden SSID").decode(errors="ignore")  # Handle missing SSID
+    sender = packet.addr2
+    receiver = packet.addr1
 
-        packetType = "Beacon" if packet.haslayer(Dot11Beacon) else "Probe"
-        macType = "Access Point" if packet.haslayer(Dot11Beacon) else "Device"
-
-        print(f"[{packetType}] Frame: SSID: {ssid} dBm, RSSI: {rssi}, {macType}: {mac}")
+    if packet.haslayer(Dot11Beacon) or packet.haslayer(Dot11ProbeReq) or packet.haslayer(Dot11ProbeResp):
+        packetType = "Beacon   " if packet.haslayer(Dot11Beacon) else "ProbeReq " if packet.haslayer(Dot11ProbeReq) else "ProbeResp"
+        print(f"[{packetType}]: RSSI: {rssi} dBm, Sender: {sender}, Receiver: {receiver}, SSID: {ssid}")
+    else:
+        print(f"[Other    ]: RSSI: {rssi} dBm, Sender: {sender}, Receiver: {receiver}, SSID: {ssid}")
 
 def start_sniffing(interface):
     print(f"Sniffing on interface {interface}...")
